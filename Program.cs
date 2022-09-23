0717a82ebcdb0b6f44c9757ff6eb5036549000e8
agostinopique <agostino.pique@gmail.com>
2022-09-23 17:03:28 +0200

    Completato esercizio;

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ try
 
     string line = file.ReadLine();
     string[] stringParts = line.Split(",");
-    
+
     string name = stringParts[0];
     string surname = stringParts[1];
     string street = stringParts[2];
@@ -39,8 +39,13 @@ try
     string province = stringParts[4];
     int zip = int.Parse(stringParts[5]);
 
+      
+
+    // Check if the address contains the home number.
+    // Turns false if the user inserts a second name or another string insteadof the address
     bool isNumeric = street.All(char.IsDigit);
 
+    // If not increase the index of each parameter to compensate for the extra parameter inserted by the user
     if (!isNumeric)
     {
         street = stringParts[3];
@@ -63,12 +68,3 @@ catch (Exception e)
 
 
 file.Close();
-
-//          Name,Surname,Street,City,Province,ZIP
-//          John, Doe,120 jefferson st., Riverside, NJ,08075
-//          Jack, McGinnis,220 hobo Av., Phila, PA,09119
-//          John Da Man, Repici,120 Jefferson St., Riverside, NJ,08075
-//          Stephen, Tyler,7452 Terrace "At the Plaza" road, SomeTown, SD,91234
-//          Joan, Anne, Jet,9th at Terrace plc, Desert City, CO,00123
-//          , Blankman,, New York, NY, 100101
-//          Joan, SomeTown, SD, 00298
